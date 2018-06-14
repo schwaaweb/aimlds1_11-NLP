48163116d778b2c86def225a6a715badcdb26477
Darwin Johnson <schwaaweb@gmail.com>
2018-06-14 14:04:29 -0400

    remove credential.py from git hub put up credentialEXAMPLE.py as a stand-in without the secrets

---
+++ credentialsEXAMPLE.py
@@ -0,0 +1,20 @@
+
+#Consumer Key (API Key) hse80rQnATyXi7lPgMXXoJRmP
+
+#Consumer Secret (API Secret) __________________________________
+
+#Access Token 3110959311-4u4cJDHgP1xUODq9ZeDul4j8YL4SIgjFkVC57VU
+
+#Access Token Secret __________________________________
+
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'hse80rQnATyXi7lPgMXXoJRmP'
+CONSUMER_SECRET = '__________________________________'
+
+# Access:
+ACCESS_TOKEN  = '3110959311-4u4cJDHgP1xUODq9ZeDul4j8YL4SIgjFkVC57VU'
+ACCESS_SECRET = '__________________________________'
+
+
